e9c763c9c97395d5f97f6fee850f74f7e3998aeb
megasloupe <139587361+megasloupe@users.noreply.github.com>
2024-06-15 18:54:39 +0300

    views

---
+++ THE Project/Views/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CoSimulationPlcSimAdv.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window 
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = new ViewModels.MainWindowViewModel(); 
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            ViewModels.MainWindowViewModel Application = this.DataContext as ViewModels.MainWindowViewModel;
+            //Unregister Instance
+            Application.virtualController.instance.UnregisterInstance();
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
